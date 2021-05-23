631769fb16a4aee64c126bb22783c76d7f701d1d
Davi de Oliveira <odavi076@gmail.com>
2021-05-23 13:25:03 -0300

    Desafio 10 Exercício 1 - 23/05/21

---
+++ src/desafio10exer01/Desafio10Exer01.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafio10exer01;
+
+/**
+ *
+ * @author odavi
+ */
+public class Desafio10Exer01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int numero = 101; numero < 200; numero = numero + 2){
+            System.out.println("Números: "+numero);
+        }
+    }
+    
+}
